de6963b8b3894a573b444f5c7de329d30ab8d97e
LucreLucre <lucrevigo@gmail.com>
2025-11-13 16:44:37 -0300

    Muestra nombre y nro socio en pagarCuota

--- app/src/main/java/com/example/clubdeportivo/PagarCuotaActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/PagarCuotaActivity.kt
@@ -3,6 +3,7 @@ package com.example.clubdeportivo
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,8 +14,16 @@ class PagarCuotaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pagar_cuota)
+        val tvNroSocio = findViewById<TextView>(R.id.tvNroSocio)
+        val nro_socio = intent.getStringExtra("numero")
+        tvNroSocio.text = "Nº de socio: $nro_socio"
 
-        val btnPagar = findViewById<Button>(R.id.btnPagar)
+        val tvnombreApellido = findViewById<TextView>(R.id.tvNombreApellido)
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        tvnombreApellido.text = "$apellido, $nombre"
+
+        val btnPagar = findViewById<Button>(R.id.btnPagarCuota)
         btnPagar.setOnClickListener {
             val intent = Intent(this, PagoCuotaRegistradoActivity::class.java)
             startActivity(intent)
