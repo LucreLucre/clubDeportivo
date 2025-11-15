192c6cc239ef15797c98046ff12eb5097106c8f8
LucreLucre <lucrevigo@gmail.com>
2025-11-15 11:25:06 -0300

    agrega fecha de pago a factura

--- app/src/main/java/com/example/clubdeportivo/FacturaDigitalCuotaSocioActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/FacturaDigitalCuotaSocioActivity.kt
@@ -1,15 +1,19 @@
 package com.example.clubdeportivo
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDate.now
 
 class FacturaDigitalCuotaSocioActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +26,9 @@ class FacturaDigitalCuotaSocioActivity : AppCompatActivity() {
         val nombre = intent.getStringExtra("nombre")
         val apellido = intent.getStringExtra("apellido")
         tvSocioFactura.text = "$apellido, $nombre"
+
+        val tvFecha = findViewById<TextView>(R.id.tvFechaFactura)
+        tvFecha.text = now().toString()
 
 
         val btnVerCarnet = findViewById<Button>(R.id.btnVerCarnet)
